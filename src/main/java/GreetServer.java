aceaf451efabf97c289ecc1edc1778814e272a3f
Siddhesh Shirsat <siddhesh.shirsat@gmail.com>
2024-08-12 08:27:32 -0700

    endpoint to talk to tcp server

---
+++ src/main/java/GreetServer.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GreetServer {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String greeting = in.readLine();
+        System.out.println("Reached..." + greeting);
+        out.println("Sending to client..." + greeting);
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+    public static void main(String[] args) {
+        GreetServer server=new GreetServer();
+        try {
+			server.start(9001);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+}
---
+++ src/main/java/com/helloworld/controller/Test123Controller.java
@@ -0,0 +1,42 @@
+package com.helloworld.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.helloworld.dto.Test123Request;
+import com.helloworld.dto.Test123Response;
+
+@RestController
+public class Test123Controller {
+	@RequestMapping("/test123")
+	public @ResponseBody Test123Response sum(@RequestBody Test123Request test123Request) {
+		String result = "";
+		
+		try (Socket socket = new Socket("ec2-52-15-82-138.us-east-2.compute.amazonaws.com", 9001)) {
+			 
+            InputStream input = socket.getInputStream();
+            InputStreamReader reader = new InputStreamReader(input);
+ 
+            int character;
+            StringBuilder data = new StringBuilder();
+ 
+            while ((character = reader.read()) != -1) {
+                data.append((char) character);
+            }
+            result = data.toString();
+        } catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+		return new Test123Response(result);
+	}
+}
